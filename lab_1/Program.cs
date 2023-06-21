c5199396fcb16b85676f1ad07ee752ca697e2a07
katri <katri@LAPTOP-HA58578M>
2023-06-22 00:18:35 +0300

    Добавьте файлы проекта.

---
+++ lab_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Чтение входного числа N из файла
+int N = int.Parse(File.ReadAllText("input.txt"));
+
+bool isFound = false;
+
+for (long q = 2; q <= Math.Sqrt(N) + 1; q++)
+{
+    if (N % q == 0)
+    {
+        long A = N / q;
+
+        // Запись найденных значений A и B в выходной файл
+        File.WriteAllText("output.txt", $"Кольцова Екатерина Владимировна\n090301-ПОВа-з21\n{A} {N - A}");
+        isFound = true;
+        break;
+    }
+}
+
+if (!isFound)
+{
+    // Запись найденных значений A и B в выходной файл
+    File.WriteAllText("output.txt", $"Кольцова Екатерина Владимировна\n090301-ПОВа-з21\n1 {(N - 1)}");
+}
